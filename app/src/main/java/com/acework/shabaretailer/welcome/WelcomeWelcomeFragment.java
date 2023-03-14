fd291e659ed44c32c31c7fa26f4726cbe4d6e6f2
Joseph Sang <jsang275@gmail.com>
2023-03-14 18:55:39 +0300

    - added animation for first slide

--- app/src/main/java/com/acework/shabaretailer/welcome/WelcomeWelcomeFragment.java
+++ app/src/main/java/com/acework/shabaretailer/welcome/WelcomeWelcomeFragment.java
@@ -7,13 +7,21 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.transition.Scene;
+import androidx.transition.Transition;
+import androidx.transition.TransitionInflater;
+import androidx.transition.TransitionManager;
 
 import com.acework.shabaretailer.R;
 import com.acework.shabaretailer.WelcomeActivity;
+import com.acework.shabaretailer.atlas.SimplifiedTL;
 import com.google.android.material.button.MaterialButton;
 
 public class WelcomeWelcomeFragment extends Fragment {
+    private ConstraintLayout root;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +30,28 @@ public class WelcomeWelcomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_welcome_welcome, container, false);
+        return inflater.inflate(R.layout.fragment_welcome_welcome_pre, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        MaterialButton s = view.findViewById(R.id.skip);
-        MaterialButton n = view.findViewById(R.id.next);
+        root = view.findViewById(R.id.root);
+        animate();
+    }
+
+    private void animate() {
+        root.postDelayed(() -> {
+            Scene next = Scene.getSceneForLayout(root, R.layout.fragment_welcome_welcome, requireContext());
+            Transition set = TransitionInflater.from(requireContext()).inflateTransition(R.transition.welcome_welcome);
+            set.addListener(SimplifiedTL.getListener(this::initialize));
+            TransitionManager.go(next, set);
+        }, 1000);
+    }
+
+    private void initialize() {
+        MaterialButton s = requireView().findViewById(R.id.skip);
+        MaterialButton n = requireView().findViewById(R.id.next);
         s.setOnClickListener(v -> ((WelcomeActivity) requireActivity()).toLogin());
         n.setOnClickListener(v -> ((WelcomeActivity) requireActivity()).toAboutUs());
     }

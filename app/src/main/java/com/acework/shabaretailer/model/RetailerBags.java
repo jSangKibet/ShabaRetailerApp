139d5f23b2f4c44f64785ec1bef2dbc19beec962
Joseph Sang <jsang275@gmail.com>
2023-05-04 20:46:17 +0300

    On sign up, retailer bags are created
    Signed-off-by: Joseph Sang <jsang275@gmail.com>

---
+++ app/src/main/java/com/acework/shabaretailer/model/RetailerBags.java
@@ -0,0 +1,34 @@
+package com.acework.shabaretailer.model;
+
+public class RetailerBags {
+    private int twendeConsignment;
+    private int sawaConsignment;
+    private int wahuraConsignment;
+    private int twendeCommission;
+    private int sawaCommission;
+    private int wahuraCommission;
+
+    public int getTwendeConsignment() {
+        return twendeConsignment;
+    }
+
+    public int getSawaConsignment() {
+        return sawaConsignment;
+    }
+
+    public int getWahuraConsignment() {
+        return wahuraConsignment;
+    }
+
+    public int getTwendeCommission() {
+        return twendeCommission;
+    }
+
+    public int getSawaCommission() {
+        return sawaCommission;
+    }
+
+    public int getWahuraCommission() {
+        return wahuraCommission;
+    }
+}

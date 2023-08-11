744c9f976d9471bd6b0f0b3c5363bb5d50ee83f0
Joseph Sang <jsang275@gmail.com>
2023-08-11 11:40:40 +0300

    A retailer can choose a box. The box chosen box is displayed in the order placement activity

--- app/src/main/java/com/acework/shabaretailer/ui/PlaceOrderActivity.java
+++ app/src/main/java/com/acework/shabaretailer/ui/PlaceOrderActivity.java
@@ -1,19 +1,26 @@
 package com.acework.shabaretailer.ui;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.acework.shabaretailer.R;
+import com.acework.shabaretailer.atlas.Atlas;
 import com.acework.shabaretailer.databinding.ActivityPlaceOrderBinding;
 import com.acework.shabaretailer.dialog.ChooseBoxDialog;
+import com.acework.shabaretailer.model.Box;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PlaceOrderActivity extends AppCompatActivity {
     ActivityPlaceOrderBinding binding;
+    private Box box;
+    private String orderType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,7 @@ public class PlaceOrderActivity extends AppCompatActivity {
         binding.chooseBox.setOnClickListener(v -> chooseBox());
 
         initializeOrderChoices();
+        setOrderTypeChangeListener();
     }
 
     private void initializeOrderChoices() {
@@ -37,10 +45,56 @@ public class PlaceOrderActivity extends AppCompatActivity {
         binding.orderTypeField.setText(getString(R.string.wholesale), false);
     }
 
-    private void chooseBox() {
-        ChooseBoxDialog dialog = ChooseBoxDialog.newInstance(box -> {
+    private void setOrderTypeChangeListener() {
+        binding.orderTypeField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-        }, binding.orderTypeField.getText().toString().trim());
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                orderType = s.toString();
+                displayBox();
+            }
+        });
+    }
+
+    private void chooseBox() {
+        ChooseBoxDialog dialog = ChooseBoxDialog.newInstance(this::boxChosen, binding.orderTypeField.getText().toString().trim());
         dialog.show(getSupportFragmentManager(), ChooseBoxDialog.TAG);
+    }
+
+    private void boxChosen(Box box) {
+        this.box = box;
+        displayBox();
+    }
+
+    private void displayBox() {
+        if (box != null) {
+            binding.name.setText(getString(R.string.box_name_ph, box.number, box.name));
+            if (box.wahura > 0) {
+                binding.wahura.setText(getString(R.string.box_wahura_ph, box.wahura));
+                binding.wahura.setVisibility(View.VISIBLE);
+            } else {
+                binding.wahura.setVisibility(View.GONE);
+            }
+            if (box.twende > 0) {
+                binding.twende.setText(getString(R.string.box_twende_ph, box.twende));
+                binding.twende.setVisibility(View.VISIBLE);
+            } else {
+                binding.twende.setVisibility(View.GONE);
+            }
+
+            int total = (Atlas.getWahuraPrice(orderType) * box.wahura) + (Atlas.getTwendePrice(orderType) * box.twende);
+            binding.total.setText(getString(R.string.box_total_ph, orderType, total));
+
+            binding.boxDetails.setVisibility(View.VISIBLE);
+        }
     }
 }

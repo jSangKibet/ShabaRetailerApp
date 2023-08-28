bf9060a493e01aee679f198b25d769128b4c5993
Joseph Sang <jsang275@gmail.com>
2023-08-28 14:42:13 +0300

    BYB intro activity launches instructions activity

--- app/src/main/java/com/acework/shabaretailer/ui/view/byb/BybIntroActivity.kt
+++ app/src/main/java/com/acework/shabaretailer/ui/view/byb/BybIntroActivity.kt
@@ -1,12 +1,19 @@
 package com.acework.shabaretailer.ui.view.byb
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.acework.shabaretailer.R
+import androidx.appcompat.app.AppCompatActivity
+import com.acework.shabaretailer.databinding.ActivityBybIntroBinding
 
 class BybIntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBybIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_byb_intro)
+        binding = ActivityBybIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.instructions.setOnClickListener {
+            startActivity(Intent(this, InstructionsActivity::class.java))
+            finish()
+        }
     }
 }

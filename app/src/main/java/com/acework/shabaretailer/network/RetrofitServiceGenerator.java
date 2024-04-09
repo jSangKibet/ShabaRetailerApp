24555c50bce7e6730572f304cd7df101d6092141
Joseph Sang <jsang275@gmail.com>
2024-04-09 09:18:19 +0300

    Pointed app to production DHL API

--- app/src/main/java/com/acework/shabaretailer/network/RetrofitServiceGenerator.java
+++ app/src/main/java/com/acework/shabaretailer/network/RetrofitServiceGenerator.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RetrofitServiceGenerator {
-    private static final String MY_DHL_API_BASE_URL = "https://express.api.dhl.com/mydhlapi/test/";
+    private static final String MY_DHL_API_BASE_URL = "https://express.api.dhl.com/mydhlapi/";
     private static final OkHttpClient.Builder httpClient = new OkHttpClient.Builder().addInterceptor(chain -> {
         Request request = chain.request().newBuilder().addHeader("Content-Type", "application/json").addHeader("Accept", "application/json").build();
         return chain.proceed(request);
